7df9372f0684606fe377c20d3cf27b8493dc7ba6
Noa16h <n.dormann@gmx.net>
2019-07-11 11:46:27 +0200

    erste Blobdetection

--- webcam.py
+++ webcam.py
@@ -1,16 +1,36 @@
 import os;
 import time;
 import cv2;
+import numpy as np;
 
 
 
 
 video = cv2.VideoCapture(0)
 
-check,frame = video.read()
 
-cv2.imshow("Capturing", frame)
+while True:
+    
+    check,frame = video.read() 
 
-cv2.waitKey('o')
+    #h,w = frame.shape
+    #im = frame.astype(np.int32)
+    im = frame
+
+    print(type(im))
+    print(type(frame))
+    detector = cv2.SimpleBlobDetector_create()
+    print(detector)
+    keypoints = detector.detect(im)
+
+    im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]),(255,0,0),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    cv2.imshow("Capturing", im_with_keypoints )
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
 
 video.release()
+cv2.destroyAllWindows()
